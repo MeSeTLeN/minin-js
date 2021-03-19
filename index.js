63f8237c1177c909ce55ab3beaf7b0e5d26feab2
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-19 17:25:15 +0200

    example 2

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-function createCalcFunc(n) {
-  return function () {
-    console.log(1000 * n);
+// function createCalcFunc(n) {
+//   return function () {
+//     console.log(1000 * n);
+//   };
+// }
+
+// const calc = createCalcFunc(42);
+// calc();
+
+function createIncrementor(n) {
+  return function (num) {
+    return n + num;
   };
 }
 
-const calc = createCalcFunc(42);
-calc();
+const addOne = createIncrementor(1);
+const addTen = createIncrementor(10);
+
+console.log(addOne(10));
+console.log(addOne(41));
+
+console.log(addTen(10));
+console.log(addTen(41));
