f4078fb6b77e17adf7a43dafb90054c174b0dcaa
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-19 12:15:39 +0200

    create obj method sayHello

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const person = {
+const person = new Object({
   name: "Maxim",
   age: 25,
   greet: function () {
     console.log("Greet!");
   },
+});
+
+Object.prototype.sayHello = function () {
+  console.log("Hello");
 };
