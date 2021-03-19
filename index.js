5361ef29919d4a3d13f197783a51b5175cd25547
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-19 21:11:20 +0200

    example 2 callback stack & queue

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ console.log("Start");
 
 console.log("Start 2");
 
+function timeout2sec() {
+  console.log("timeout2sec");
+}
+
 window.setTimeout(() => {
   console.log("Inside timeout, after 2 sec");
 }, 2000);
 
+setTimeout(timeout2sec, 1000);
+
 console.log("End");
+
+setTimeout(() => {
+  console.log("Queue cause async");
+}, 0);
+
+// google loupe js
