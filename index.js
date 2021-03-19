1b871dcd8ad41f3cf9fe7eef6524dbf4ddbc9dec
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-19 16:22:00 +0200

    bind

--- index.js
+++ index.js
@@ -6,4 +6,23 @@ const person = {
   name: "Maksym",
   age: 24,
   sayHello: hello,
+  sayHelloWindowShort: hello.bind(this),
+  sayHelloWindow: hello.bind(window),
+  sayHelloWindowDocument: hello.bind(document),
+  logInfo: function (job, phone) {
+    console.group(`${this.name} info:`);
+    console.log(`Name is ${this.name}`);
+    console.log(`Age is ${this.age}`);
+    console.log(`Job is ${job}`);
+    console.log(`Phone is ${phone}`);
+    console.groupEnd();
+  },
 };
+
+const lena = {
+  name: "Elena",
+  age: 23,
+};
+
+const fnLenaInfoLog = person.logInfo.bind(lena);
+fnLenaInfoLog("Frontend", "+380-99-99-99-999");
