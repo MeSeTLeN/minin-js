048774ef90b66c0519846d809c7412248a8115a7
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-19 17:14:54 +0200

    prototype pract

--- index.js
+++ index.js
@@ -27,4 +27,20 @@ const lena = {
 // const fnLenaInfoLog = person.logInfo.bind(lena);
 // fnLenaInfoLog("Frontend", "+380-99-99-99-999");
 // person.logInfo.call(lena, "Frontend", "+380-99-99-99-999");
-person.logInfo.apply(lena, ["Frontend", "+380-99-99-99-999"]);
+// person.logInfo.apply(lena, ["Frontend", "+380-99-99-99-999"]);
+
+const array = [1, 2, 3, 4, 5];
+
+// function multBy(arr, n) {
+//   return arr.map(function (i) {
+//     return i * n;
+//   });
+// }
+
+Array.prototype.multBy = function (n) {
+  return this.map(function (i) {
+    return i * n;
+  });
+};
+
+console.log(array.multBy(20));
