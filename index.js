d78d9f876550b60b9871ede0ee86c2ba5bb3020a
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-19 17:29:08 +0200

    example 3

--- index.js
+++ index.js
@@ -7,17 +7,34 @@
 // const calc = createCalcFunc(42);
 // calc();
 
-function createIncrementor(n) {
-  return function (num) {
-    return n + num;
+// function createIncrementor(n) {
+//   return function (num) {
+//     return n + num;
+//   };
+// }
+
+// const addOne = createIncrementor(1);
+// const addTen = createIncrementor(10);
+
+// console.log(addOne(10));
+// console.log(addOne(41));
+
+// console.log(addTen(10));
+// console.log(addTen(41));
+
+function urlGenerator(domain) {
+  return function (url) {
+    return `https://${url}.${domain}`;
   };
 }
 
-const addOne = createIncrementor(1);
-const addTen = createIncrementor(10);
+const comUrl = urlGenerator("com");
+const ruUrl = urlGenerator("ru");
 
-console.log(addOne(10));
-console.log(addOne(41));
+console.log(comUrl("google"));
+console.log(comUrl("netflix"));
 
-console.log(addTen(10));
-console.log(addTen(41));
+console.log(ruUrl("yandex"));
+console.log(ruUrl("vkontakte"));
+
+console.log(comUrl("vkontakte"));
